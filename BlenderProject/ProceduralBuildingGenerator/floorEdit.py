5ef1e857695e103f471826705fb594f5e2b25fc8
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2021-04-15 15:02:06 +0200

    Added Blender Project

---
+++ BlenderProject/ProceduralBuildingGenerator/floorEdit.py
@@ -0,0 +1,47 @@
+import bpy  
+from bpy.types import Operator  
+from bpy.props import FloatVectorProperty, FloatProperty  
+  s
+class ScaleOperator(bpy.types.Operator):  
+ """Scale Operator"""  
+ bl_idname = "object.scale_operator"  
+ bl_label = "Scale Operator"  
+ bl_options = {'REGISTER', 'UNDO'}  
+  
+ scale = FloatVectorProperty(  
+   name="Scale",  
+   default=(1.0, 1.0, 1.0),  
+   subtype='XYZ',  
+   description="Scale the building floor size"  
+   )  
+   
+ floorNum = IntProperty(  
+   name="Floor Number",  
+   default=1.0,  
+   subtype='DISTANCE',  
+   unit='LENGTH',  
+   description="Choose the number of floors the building will have"  
+   )  
+  
+ def execute(self, context):  
+  scale = self.scale
+  context.active_object.scale *= scale
+  return {'FINISHED'}  
+  
+ @classmethod  
+ def poll(cls, context):  
+  ob = context.active_object  
+  return ob is not None and ob.mode == 'OBJECT'  
+  
+def add_object_button(self, context):  
+ self.layout.operator(  
+  Move3Operator.bl_idname,  
+  text= "Hola",  
+  icon='PLUGIN')  
+  
+def register():  
+ bpy.utils.register_class(ScaleOperator)  
+ bpy.types.VIEW3D_MT_object.append(add_object_button)  
+  
+if __name__ == "__main__":  
+ register()  
---
+++ BlenderProject/ProceduralBuildingGenerator/panelController.py
@@ -0,0 +1,91 @@
+bl_info = {
+    "name" : "Procedural Building Generator",
+    "author" : "Alex Morales Garcia",
+    "version" : 1.0,
+    "location" : "View3d > Tool",
+    "warning" : "",
+    "wiki_url" : "",
+    "category" : "Procedural",
+}
+
+import bpy
+
+
+
+class MainPanelPBG(bpy.types.Panel):
+    bl_label = "Procedural Building Generator"
+    bl_idname = "NODE_PT_MAINPANELPBG"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = 'PBG'
+
+    def draw(self, context):
+        layout = self.layout
+        
+        row = layout.row()
+        row.label(text="Floor", icon='OBJECT_ORIGIN')
+        
+        row = layout.row()
+        row.operator(ScaleOperator.bl_idname)
+        
+        row = layout.row()
+        # Check if floor is created
+        floorObj = bpy.context.scene.objects.get("Floor")
+        if(floorObj):
+            row.operator(AddFloor.bl_idname, text="Modify Floor", icon='CUBE')
+        else:
+            row.operator(AddFloor.bl_idname, text="Generate Building", icon='CUBE')
+
+def addFloor(context, operator, size):
+    
+    # Generate a new cube
+    bpy.ops.mesh.primitive_cube_add(scale=(size[0], size[1], size[2]))
+    
+    # Get created cube
+    cube = bpy.context.selected_objects[0]
+    
+    # Modify name
+    cube.name = "Floor"
+    
+def modifyFloor(context, operator, size):
+    
+    # Get created cube
+    cube = bpy.context.selected_objects[0]
+    
+    # Change scale
+    cube.scale = size;
+        
+        
+class AddFloor(bpy.types.Operator):
+    bl_label = "Add Floor with parameters"
+    bl_idname = "pbg.addfloor"
+    
+    def execute(self, context):
+        
+        floorObj = bpy.context.scene.objects.get("Floor")
+        if(floorObj):
+            floorSize = [1.0, 2.0, 1.0]
+            
+            bpy.data.objects['Floor'].select_set(True)
+            modifyFloor(self, context, floorSize)
+        else:
+            floorSize = [1.0, 2.0, 1.0]
+            
+            addFloor(self, context, floorSize)
+            
+        
+        return{'FINISHED'}
+
+
+def register():
+    bpy.utils.register_class(MainPanelPBG)
+    bpy.utils.register_class(AddFloor)
+
+
+def unregister():
+    bpy.utils.unregister_class(MainPanelPBG)
+    bpy.utils.register_class(AddFloor)
+
+
+if __name__ == "__main__":
+    register()
